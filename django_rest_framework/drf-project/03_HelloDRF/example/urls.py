62a8a31913ee136d2232aa7be30ff7b9795ef397
jher235 <tim668666@gmail.com>
2023-07-05 18:58:47 +0900

    간단한 도서 목록 drf 구현

---
+++ django_rest_framework/drf-project/03_HelloDRF/example/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path, include
+from rest_framework import routers
+from .views import *
+
+
+urlpatterns = [
+    path('hello/', HelloAPI),
+    path('fbv/books/', booksAPI),
+    path('fbv/book/<int:bid>/', bookAPI),
+    path('cbv/books/', BooksAPI.as_view()),
+    path('cbv/book/<int:bid>/', BookAPI.as_view()),
+    path('mixin/books/', BooksAPIMixins.as_view()),
+    path('mixin/book/<int:bid>/', BookAPIMixins.as_view()),
+    path('generics/',BooksAPIGenerics.as_view()),
+    path('generics/<int:bid>/',BookAPIGenerics.as_view()),
+]
+
+# router = routers.SimpleRouter()
+# router.register('books',BookViewSet)
+#
+# urlpatterns = router.urls
